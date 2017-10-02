f11c8552d6f16a69228c907f88d6ef19fc12fdd0
pol-Gina  <gina.sie@polstargps.com>
2017-10-02 10:06:39 +0800

    ttt

---
+++ testObjectC/testObjectC/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  testObjectC
+//
+//  Created by polstar on 2017/9/29.
+//  Copyright © 2017年 polstar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"123");
+        NSLog(@"123");
+        NSLog(@"123");    NSLog(@"123");
+    
+        NSLog(@"123");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ testObjectC/testObjectC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testObjectC
+//
+//  Created by polstar on 2017/9/29.
+//  Copyright © 2017年 polstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
